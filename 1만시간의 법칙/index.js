19d9e0d8f83b823b2124c0977fa84a0357ff6ba9
yooss2006 <yooss2006@naver.com>
2021-12-12 20:26:42 +0900

    create 1만시간의 법칙 자바스크립트 적용

---
+++ 1만시간의 법칙/index.js
@@ -0,0 +1,29 @@
+let expert = document.querySelector("#expert");
+let hour = document.querySelector("#hour");
+let result = document.querySelector(".inp-btn");
+let 연산 = 0;
+
+result.addEventListener("click", () => {
+  if (expert.value && hour.value) {
+    if (document.querySelector(".hide-result").style.display != "block") {
+      document.querySelector(".hide-result").style.display = "block";
+    } else {
+      document.querySelector(".hide-result").style.display = "none";
+    }
+  } else {
+    alert("입력란을 작성해주세요");
+  }
+});
+
+expert.addEventListener("change", function () {
+  document.querySelector("#expert-come").innerHTML = expert.value;
+});
+hour.addEventListener("change", function () {
+  if (hour.value < 1) {
+    hour.value = 1;
+  } else if (hour.value > 24) {
+    hour.value = 24;
+  }
+  연산 = Math.floor(10000 / hour.value);
+  document.querySelector("#hour-come").innerHTML = 연산;
+});
